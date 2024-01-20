04c99502a70e5e2d5937908b1c76b5152f183f59
thuankg1752 <thuankg1752@gmail.com>
2024-01-20 21:11:00 +0700

    wip: ui home page

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
 import './scss/styles.scss';
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    let checkbox = document.querySelector("#toggleSystemMode") as HTMLInputElement;
+    let body = document.querySelector("body");
+    if (!checkbox || !body) return;
+
+    checkbox.addEventListener('change', (event) => {
+        const target = event.target as HTMLInputElement;
+        target.checked ? body.setAttribute('data-theme', "dark") : body.removeAttribute("data-theme");
+    });
+});
